cdf308c50404755f091dfbdf2599921b8a30da32
Yan Magalhães <yanstrangerb@gmail.com>
2025-06-05 11:21:42 -0300

    Update BancoDados.sql

--- Trabalho POO/BancoDados.sql
+++ Trabalho POO/BancoDados.sql
@@ -1,1 +1,8 @@
-SELECT * FROM filme.filmes;
+create database filme;
+use filme;
+
+CREATE TABLE filmes (
+    id INT(4) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(50),
+    sinopse VARCHAR(150)
+);
